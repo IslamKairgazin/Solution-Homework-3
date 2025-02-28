b01fd3c81fbbb5204e51c14eb6432971aa91c07d
Islam Kairgazin <islam.kairgazin@narxoz.kz>
2025-02-28 21:07:12 +0500

    Delete src/main/java/IWorldBuilder.java

--- src/main/java/IWorldBuilder.java
+++
@@ -1,6 +0,0 @@
-public interface IWorldBuilder {
-    IWorldBuilder setWorldName(String name);
-    IWorldBuilder addBiome(String biome);
-    IWorldBuilder addStructure(String structure);
-    World build();
-}
