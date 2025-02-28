b35de0c5c748b97fb47285193e3c8df668c35e31
Islam Kairgazin <islam.kairgazin@narxoz.kz>
2025-02-28 21:33:24 +0500

    MUDPrototypeDemo.java

--- src/main/java/MUDPrototypeDemo.java
+++ src/main/java/MUDPrototypeDemo.java
@@ -1,15 +1,21 @@
+// Класс для демонстрации работы паттерна Prototype
 public class MUDPrototypeDemo {
     public static void main(String[] args) {
-
-        Block stone = new Block("Камень");
-        Block clonedStone = (Block) stone.cloneEntity();
-
+        // Создаём оригинальные объекты
+        Block stoneBlock = new Block("Камень");
         Entity cow = new Entity("Корова", 10);
+
+        // Клонируем объекты
+        Block clonedBlock = (Block) stoneBlock.cloneEntity();
         Entity clonedCow = (Entity) cow.cloneEntity();
 
-        System.out.println(stone);
-        System.out.println(clonedStone);
+        // Выводим оригиналы и их клоны
+        System.out.println("Оригиналы:");
+        System.out.println(stoneBlock);
         System.out.println(cow);
+
+        System.out.println("\nКлоны:");
+        System.out.println(clonedBlock);
         System.out.println(clonedCow);
     }
 }
